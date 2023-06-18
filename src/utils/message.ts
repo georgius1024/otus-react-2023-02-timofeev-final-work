229eb43272f4882b6834e87839d4e747b9496b9f
georgius1024 <georgius@ural.ru>
2023-06-18 23:32:10 +0500

    better messages

--- src/utils/message.ts
+++ src/utils/message.ts
@@ -1,7 +1,7 @@
 export function success(message: string): void {
-  setTimeout(() => alert(`Success: ${message}`), 0);
+  setTimeout(() => alert(`Success: ${message}`), 10);
 }
 
 export function warning(message: string): void {
-  setTimeout(() => alert(`Warning: ${message}`), 0);
+  setTimeout(() => alert(`Warning: ${message}`), 10);
 }
