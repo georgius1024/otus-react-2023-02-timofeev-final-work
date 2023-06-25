e2a691197a7288bb1d863eb8b734786863288c62
georgius1024 <georgius@ural.ru>
2023-06-25 17:13:14 +0500

    module crud

--- src/pages/Module.tsx
+++ src/pages/Module.tsx
@@ -1,44 +1,51 @@
 import { ReactElement, useEffect, useState, useCallback } from "react";
 import type { Module } from "@/types";
 import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import "@/pages/Module.scss";
 
 //import useAlert from "@/utils/AlertHook";
 import useBusy from "@/utils/BusyHook";
+import FormGroup from "@/components/FormGroup";
+import FormInput from "@/components/FormInput";
 import * as modules from "@/services/modules";
 export default function ModulePage(): ReactElement {
   const [childrenModules, setChildrenModules] = useState<Module[]>([]);
   const [currentModule, setCurrentModule] = useState<Module | null>(null);
   const [parentModules, setParentModules] = useState<Module[]>([]);
+  const [newName, setNewName] = useState<string>("");
   //const alert = useAlert();
   const busy = useBusy();
   const { id = "" } = useParams();
+  const navigate = useNavigate();
 
   const reload = useCallback(
     async (parent: string) => {
       busy(true);
 
-      const fetchCurrent = (): "" | Promise<Module | null> => parent && modules.fetchOne(parent);
-      const fetchChildren = (): Promise<Module[]> => modules.fetchChildren(parent);
-      const fetchParents = async (): Promise<Module[]> => {
-        let current = currentModule
-        const path:Module[] = []
+      const fetchChildren = (id: string): Promise<Module[]> =>
+        modules.fetchChildren(id);
+      const fetchPath = async (id: string): Promise<Module[]> => {
+        let current = id;
+        const path: Module[] = [];
         while (current) {
-          const parent = await modules.fetchOne(current.parent)
-          parent && path.push(parent)
-          current = parent
+          const module = await modules.fetchOne(current);
+          module && path.push(module);
+          current = module?.parent || "";
         }
-        return path
-      }
+        return [...path].reverse();
+      };
 
-      Promise.all([fetchCurrent(), fetchChildren(), fetchParents()])
+      Promise.all([fetchChildren(parent), fetchPath(parent)])
         .then((response) => {
-          setCurrentModule(response[0] || null);
-          setChildrenModules(response[1]);
-          return fetchParents();
+          setChildrenModules(response[0]);
+          const path = response[1] || [];
+          const current = path.at(-1) || null;
+          setCurrentModule(current);
+          setParentModules(path.slice(0, -1));
+          setNewName(current?.name || "");
+          busy(false);
         })
-        .then((parents) => setParentModules(parents))
-        .then(() => busy(false))
         .catch(console.error);
     },
     [busy]
@@ -47,25 +54,155 @@ export default function ModulePage(): ReactElement {
     reload(id);
   }, [reload, id]);
 
-  const create = () => {
-    modules
-      .create({ name: "Module", type: "lesson", parent: id })
-      .catch(console.error)
-      .then(console.log);
+  const newNameChanged = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setNewName(event.target.value);
   };
 
+  const create = (type: string) => {
+    const name = prompt(`Enter new ${type} name`, "new");
+    if (!name) {
+      return;
+    }
+    busy(true);
+    modules
+      .create({ name, type, parent: id })
+      .catch(console.error)
+      .then((newModule) => {
+        newModule && navigate(`/module/${newModule.id}`);
+      })
+      .catch(console.error)
+      .finally(() => busy(false));
+  };
+
+  const saveModule = () => {
+    if (!currentModule) {
+      return;
+    }
+    const updated = { ...currentModule, name: newName } as Module;
+    busy(true);
+    modules
+      .update(updated)
+      .then(() => setCurrentModule(updated))
+      .then(() => {
+        navigate(`/module/${updated.parent}`);
+      })
+      .catch(console.error)
+      .finally(() => busy(false));
+  };
+
+  const destroyModule = () => {
+    if (!currentModule) {
+      return;
+    }
+    busy(true);
+    modules
+      .destroy(currentModule)
+      .then(() => {
+        navigate(`/module/${currentModule.parent}`);
+      })
+      .catch(console.error)
+      .finally(() => busy(false));
+  }
+  const breadcrumbs = () => {
+    const root = { name: "Root", id: "" };
+    const current = currentModule
+      ? { name: currentModule.name, id: currentModule.id }
+      : null;
+    return [
+      root,
+      ...parentModules.map((e) => ({ name: e.name, id: e.id })),
+      current,
+    ]
+      .filter(Boolean)
+      .map((e, index, array) => {
+        const isLast = index === array.length - 1;
+        return { ...e, active: isLast };
+      });
+  };
   return (
-    <>
-      {currentModule ? <h1>{currentModule.name}</h1> : <h1>Modules</h1>}
-      <ul>
-        {childrenModules.map((e) => (
-          <li key={e.id}>
-            <Link to={`/module/${e.id}`}>{e.name}</Link>
-          </li>
-        ))}
-      </ul>
-      {id && <Link to="/module">Start</Link>}
-      <button onClick={create}>Create</button>
-    </>
+    <div className="container-fluid module-page">
+      <h1>Modules</h1>
+      <nav>
+        <ol className="breadcrumb">
+          {breadcrumbs().map((e) =>
+            e.active ? (
+              <li className="breadcrumb-item active" key={e.id}>
+                {e.name}
+              </li>
+            ) : (
+              <li
+                className={`breadcrumb-item ${e.active ? "active" : ""}`}
+                key={e.id}
+              >
+                <Link to={`/module/${e.id}`}>{e.name}</Link>
+              </li>
+            )
+          )}
+        </ol>
+      </nav>
+      <div className="row">
+        <div className="col">
+          <div className="list-group modules-list">
+            {childrenModules.map((e) => (
+              <Link
+                to={`/module/${e.id}`}
+                className="list-group-item"
+                key={e.id}
+              >
+                {e.name}
+              </Link>
+            ))}
+          </div>
+          <div className="dropdown">
+            <button
+              className="btn btn-primary dropdown-toggle mt-3 w-100"
+              type="button"
+              id="createButton"
+              data-bs-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Create
+            </button>
+            <div className="dropdown-menu" aria-labelledby="createButton">
+              <div className="dropdown-item" onClick={() => create("course")}>
+                Course
+              </div>
+              <div className="dropdown-item" onClick={() => create("lesson")}>
+                Lesson
+              </div>
+              <div className="dropdown-item" onClick={() => create("activity")}>
+                Activity
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={`col ${currentModule ? "d-block" : "d-none"}`}>
+          <div className="card">
+            <div className="card-body module-editor">
+              <FormGroup label="Name">
+                <FormInput
+                  type="text"
+                  value={newName}
+                  onInput={newNameChanged}
+                />
+              </FormGroup>
+            </div>
+          </div>
+          <div className="mt-3 d-flex justify-content-end">
+            <button
+              className="btn btn-primary w-100 me-3"
+              type="button"
+              onClick={saveModule}
+            >
+              Save
+            </button>
+            <button className="btn btn-danger" type="button" onClick={destroyModule}>
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
