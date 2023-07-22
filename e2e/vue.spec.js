b8539884c2630d0400c6e49a4df4063f09fe417d
georgius1024 <georgius@ural.ru>
2023-07-22 22:08:52 +0500

    first e2e specs

--- e2e/vue.spec.js
+++ e2e/vue.spec.js
@@ -1,6 +1,35 @@
 import { test, expect } from '@playwright/test';
 
-test('visits the app root url', async ({ page }) => {
+// test('visits the app root url', async ({ page }) => {
+//   await page.goto('/');
+//   expect(await page.title()).toBe('My LMS');
+// })
+
+
+test('login', async ({ page }) => {
   await page.goto('/');
   expect(await page.title()).toBe('My LMS');
+  await page.getByRole('menu').click();
+  await page.getByRole('menuitem', {name: '🇺🇸'}).click()
+
+  const loginLink = await page.getByRole('link', {name: 'Login'})
+
+  await loginLink.click()
+
+  await page.waitForURL('**/login');
+
+  await page.getByPlaceholder('name@example.com').click();
+  await page.getByPlaceholder('name@example.com').fill('georgius@ural.ru');
+
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').fill('2vAfi5dERDSxj7y');
+
+  await page.getByRole('button', { name: 'Login' }).click();
+  
+  await page.waitForURL('**/');
+
+  await page.getByText('Home page').waitFor()
+  //await new Promise(() => {})
+  expect(await page.getByText('Home page')).toBeVisible();
+
 })
