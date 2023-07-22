646089540c7889b6d8d458ad172eb4b047ff2a67
georgius1024 <georgius@ural.ru>
2023-07-22 19:49:00 +0500

    improved tests

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,7 @@
-import { ReactElement } from "react";
-import Placeholder from "@/components/Placeholder";
-// import { ReactElement, useState } from "react";
-// import { Link } from "react-router-dom";
-// import { ReactSortable } from "react-sortablejs";
-// import useAlert from "@/utils/AlertHook";
-// import useBusy from "@/utils/BusyHook";
-// import SidePanel from "@/components/SidePanel";
-// import ModalPanel from "@/components/ModalPanel";
-// type Item = {
-//   id: number;
-//   name: string;
-// };
-export default function HomePage(): ReactElement {
+export default function HomePage() {
   return (
     <div className="container-fluid mt-4">
-      <div className="row">
-        <div className="col">
-          <Placeholder width="100%" rounded/>
-        </div>
-      </div>
+      <h1>Home page</h1>
     </div>
   )
 }
